c9632ba0edaec5679d4e66aabcf678140ffdb3e5
Patrick Tran <patrick.tran06@gmail.com>
2018-09-23 15:34:06 -0700

    add cheatsheet for creating a foreign key migration

---
+++ sequelize/migration-createColumn-foreignKey.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/**
+ * This adds a column on the SOURCE model for TARGET id in a 1:M relationship
+ */
+
+// This is the name of your "source" model that is the (1) in the relationship
+const SOURCE = 'Task'; 
+
+// This is the name of your "target" model that is the (M) in the relationship
+const TARGET = 'User';
+
+// By default we will use the field name (TARGET model + "Id")
+const COLUMN_NAME = TARGET + 'Id';
+
+module.exports = {
+  up: function(migration, DataTypes, done) {
+
+    migration.addColumn(
+      SOURCE,
+      COLUMN_NAME,
+      {
+        type: DataTypes.UUID,
+        references: { 
+          model: TARGET, 
+          key: "id" 
+        },
+        onDelete: 'cascade'
+      }
+    );
+    done();
+  },
+
+  down: function(migration, DataTypes, done) {
+    migration.removeColumn(SOURCE, COLUMN_NAME);
+    done();
+  }
+};
