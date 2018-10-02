1f584af242692a134fc1498deb959251629e17ba
Patrick Tran <patrick.tran06@gmail.com>
2018-10-02 07:45:58 -0700

    update migration create column for foreign key

--- sequelize/migration-createColumn-foreignKey.js
+++ sequelize/migration-createColumn-foreignKey.js
@@ -2,29 +2,38 @@
 
 /**
  * This adds a column on the SOURCE model for TARGET id in a 1:M relationship
+ *
  * An example is a 1:M relationship between "Task" and "User" where a "Task" 
  * belongs to a single "User" 
+ * 
+ * SOURCE (1) -> TARGET (M)
+ *           |-> TARGET (M)
+ *           |-> TARGET (M)
  */
 
+// === SET THESE ===
 // This is the name of your "source" model that is the (1) in the relationship
-const SOURCE = 'Task'; 
 
-// This is the name of your "target" model that is the (M) in the relationship
-const TARGET = 'User';
+const SOURCE_MODEL_NAME = 'Task'; 
+const TARGET_MODEL_NAME = 'User'; 
 
+// === DO NOT SET THESE ===
+// Get the real table name by initializing a sequelize instance
 // By default we will use the field name (TARGET model + "Id")
-const COLUMN_NAME = TARGET + 'Id';
+
+const SOURCE_TABLE_NAME = require(__dirname + '/../models')[SOURCE_MODEL_NAME].getTableName()
+const TARGET_FIELD_NAME = TARGET_MODEL_NAME + 'Id'
 
 module.exports = {
   up: function(migration, DataTypes, done) {
 
     migration.addColumn(
-      SOURCE,
-      COLUMN_NAME,
+      SOURCE_TABLE_NAME,
+      TARGET_FIELD_NAME,
       {
-        type: DataTypes.UUID,
+        type: DataTypes.INTEGER,
         references: { 
-          model: TARGET, 
+          model: TARGET_MODEL_NAME, 
           key: "id" 
         },
         onDelete: 'cascade'
@@ -34,7 +43,8 @@ module.exports = {
   },
 
   down: function(migration, DataTypes, done) {
-    migration.removeColumn(SOURCE, COLUMN_NAME);
+    migration.removeColumn(SOURCE_TABLE_NAME, TARGET_FIELD_NAME);
     done();
   }
 };
+
